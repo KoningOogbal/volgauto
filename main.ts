6fcb14293fc7cf2366bc8d3350b4617b06cc789c
KoningOogbal <113787669+KoningOogbal@users.noreply.github.com>
2022-11-15 21:04:23 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 let nieuw = 0
 let oud = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 107)
 basic.forever(function () {
     nieuw = radio.receivedPacket(RadioPacketProperty.SignalStrength)
     if (oud < nieuw) {
-    	
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 107)
     } else {
-    	
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     }
     oud = nieuw
 })
